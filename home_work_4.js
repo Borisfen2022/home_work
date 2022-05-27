0c1d460f37689f4d2e32df7319cbaed688ff4038
Olya_Koshelnikova <olha.koshelnikova@gmail.com>
2022-05-27 19:12:44 +0300

    undo home work 4

--- home_work_4.js
+++ home_work_4.js
@@ -1,25 +1,19 @@
-// let firstNumber = +prompt("Enter first Number, please", 0);
-// let operation = prompt("Enter operation");
-// const secondNumber = +prompt("Enter the second number", 0);
+let firstNumber = +prompt("Enter first Number, please", 0);
+let operation = prompt("Enter operation");
+const secondNumber = +prompt("Enter the second number", 0);
 
-// if (isNaN(firstNumber) || isNaN(secondNumber)) {
-//   alert("Wrong data");
-// } else {
-//   if (operation === "+") {
-//     alert(`Your result ${firstNumber + secondNumber}`);
-//   } else if (operation === "-") {
-//     alert(firstNumber - secondNumber);
-//   } else if (operation === "*") {
-//     alert(firstNumber * secondNumber);
-//   } else if (operation === "/") {
-//     alert(firstNumber / secondNumber);
-//   } else {
-//     alert("Incorrect operation");
-//   }
-// }
-
-let hour = 20;
-
-if (hour < 10 || hour > 18) {
-  alert("Офис закрыт.");
+if (isNaN(firstNumber) || isNaN(secondNumber)) {
+  alert("Wrong data");
+} else {
+  if (operation === "+") {
+    alert(`Your result ${firstNumber + secondNumber}`);
+  } else if (operation === "-") {
+    alert(firstNumber - secondNumber);
+  } else if (operation === "*") {
+    alert(firstNumber * secondNumber);
+  } else if (operation === "/") {
+    alert(firstNumber / secondNumber);
+  } else {
+    alert("Incorrect operation");
+  }
 }
